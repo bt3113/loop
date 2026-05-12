2c7ecefd690e76d686c2194432461e9fc63664db
BT <tekwanib7@gmail.com>
2026-05-12 23:37:27 +0100

    Configure static export for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true
+  },
+  basePath: process.env.NODE_ENV === "production" ? "/loop" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/loop/" : "",
+  trailingSlash: true
+};
 
 export default nextConfig;
